7e26468dd7f78433905012295dbf5428fca50b26
Johnny Tsang <jaytsang9@gmail.com>
2019-11-18 01:28:50 -0500

    add scraper for ratings and convert the rating attribute to integer form

--- book_scraper.py
+++ book_scraper.py
@@ -6,5 +6,13 @@ response = requests.get("http://books.toscrape.com/catalogue/category/books/hist
 soup = BeautifulSoup(response.text, "html.parser")
 books = soup.find_all("article")
 for book in books:
-    print(book.find("h3").find("a")["title"])
+    title = book.find("h3").find("a")["title"]
+    price = book.select(".price_color")[0].get_text()
+    price = float(price.replace("£", "").replace("Â", ""))
+    ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    paragraph = book.select(".star-rating")[0]
+    rating = paragraph.get_attribute_list("class")[-1]
+    int_rating = ratings[rating]
+    
+    
 
