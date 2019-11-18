001414b17ae3854fe89fcb5ee02560cd72cd3112
Johnny Tsang <jaytsang9@gmail.com>
2019-11-18 01:36:17 -0500

    refactor code into separate scrape methods

--- book_scraper.py
+++ book_scraper.py
@@ -2,17 +2,29 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("http://books.toscrape.com/catalogue/category/books/history_32/index.html")
-soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all("article")
+
 for book in books:
-    title = book.find("h3").find("a")["title"]
+
+def scrape_books(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article")
+    all_books = []
+    for book in books: 
+        book_data = (get_title(book), get_price(book), get_rating(book))
+        all_books.append(book_data)
+        
+def get_title(book):
+    return book.find("h3").find("a")["title"]
+
+def get_price(book):
     price = book.select(".price_color")[0].get_text()
-    price = float(price.replace("£", "").replace("Â", ""))
+    return float(price.replace("£", "").replace("Â", ""))
+
+def get_rating():
     ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
     paragraph = book.select(".star-rating")[0]
-    rating = paragraph.get_attribute_list("class")[-1]
-    int_rating = ratings[rating]
-    
-    
+    word_rating = paragraph.get_attribute_list("class")[-1]
+    return ratings[word_rating]
 
+scrape_books("http://books.toscrape.com/catalogue/category/books/history_32/index.html")
